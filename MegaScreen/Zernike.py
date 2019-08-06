b3c6c49228458df0b8e6033d18e3b401b4bbe5ea
dbuscher <dfb@mrao.cam.ac.uk>
2019-08-06 18:05:19 +0100

    Orthoganalise tested.
    X & Y coordinates of grid swapped to be consistent with matplotlib etc.

--- MegaScreen/Zernike.py
+++ MegaScreen/Zernike.py
@@ -12,116 +12,122 @@ def NumZernike(m):
     return (m + 1) * (m + 2) // 2
 
 
-
 def jtonm(j):
-    """Converts Noll index j to indices n and m.  The method is described by V. N. Mahajan, Chapter 13, Optical Shop Testing, 3rd. Ed., D. Malacara. ed., Wiley,2007"""
+    """Convert Noll index j (1-based) to radial index n and azimuthal index m.  
+
+    The method is described by V. N. Mahajan, Chapter 13, 
+    Optical Shop Testing, 3rd. Ed., D. Malacara. ed., Wiley,2007"""
     n = int(sqrt(2 * j - 1) + 0.5) - 1
-    if n % 2 == 0:
-        m = 2 * int((2 * j + 1 - n * (n + 1)) / 4.0)
-    else:
-        m = 2 * int((2 * (j + 1) - n * (n + 1)) / 4.0) - 1
-    if j % 2 != 0:
-        m = -m
+    m = (1 - 2 * (j % 2)) * (
+        2 * int((2 * j + 1 + (n % 2) - n * (n + 1)) / 4.0) - (n % 2)
+    )
     return (n, m)
 
-#@functools.lru_cache()
-def ZernikeGrid(gridSize, maxRadial, diameter=None):
+
+@functools.lru_cache()
+def ZernikeGrid(gridSize, maxRadial, diameter=None, orthoganalise=True):
+    """Return all the Zernike polynomials up to a given radial order on a square grid
+
+    If diameter is None, then the unit circle just touches the outer edges of the 
+    square (with the convention that the square extends +-0.5 pixels beyond the grid).
+    Uses a recursive method to evaluate the radial polynomial.
+    Returns the polynomials in the order given by Noll (1976), but zero-based
+    """
     if diameter == None:
         diameter = gridSize
     radius = diameter / 2.0
-    x, y = np.mgrid[0:gridSize, 0:gridSize]
+    y, x = np.mgrid[0:gridSize, 0:gridSize]
     x = (x - (gridSize - 1.0) / 2.0) / radius
     y = (y - (gridSize - 1.0) / 2.0) / radius
-    return ZernikeKintner(x, y, maxRadial)
-
-def ZernikeKintner(x, y, maxRadial):
-    """Return Zernike values at a given set of x,y coords up to some maximum Noll index.  Uses modified Kintner method.  If unitNorm=True the polynomials will all have unit normalisation, otherwise they will have standard normalisation.  j is ordered first by n, with lower j corresponding to lower n, and then by m, with lower m coming first.  Discussion may be found in 
-  'Chong, Chee-Way, P. Raveendran, and R. Mukundan. 'A Comparative Analysis of Algorithms for Fast Computation of Zernike Moments'.  Pattern Recognition 36, no. 3 (March 2003): 731-42. doi:10.1016/S0031-3203(02)00091-2.'"""
-
-    # Set bounds on indices j, n, m, p, q
+    # Convert to polar co-ordinates
+    temp = x + 1j * y
+    r = np.abs(temp)
+    R = KinterRadial(r, maxRadial)
+    # Create entheta[n] = exp(i*n*theta) recursively from exp(i*theta)
+    eitheta = np.where(r == 0.0, 1.0, temp / r)
+    entheta = np.concatenate(
+        (
+            np.ones((1,) + eitheta.shape, dtype=np.complex),
+            np.cumprod(np.broadcast_to(eitheta, (maxRadial,) + eitheta.shape), axis=0),
+        )
+    )
+    cntheta = entheta.real
+    sntheta = entheta.imag
     jmax = NumZernike(maxRadial)
-    nmmax = jtonm(jmax)
-    nmax = nmmax[0]
-    mmax = nmmax[1]
-    pmax = nmax
-    qmax = int(np.absolute(mmax))
-    # Dummy variable for holding max value of p or q in specific case
-    dmax = 0
-    # Storage array for radial and Zernike functions
-    # Creates array far larger than necessary, 200*200 for ~ 200 elements but numpy and linux employ lazy memory allocation so only the memory that is used is allocated.  This comes at a cost, if you use more memory than is available the program will crash at runtime.
-    R = np.zeros((pmax + 1, pmax + 1) + x.shape)
-    Z = np.zeros((jmax,) + x.shape)
-    # Polar co-ordinates
-    r = sqrt(x ** 2 + y ** 2)
-    phi = np.arctan2(y, x)
-    # Calc radial part of Zernikes
-    # Calc diagonal elements first
-    if qmax == pmax - 2 or qmax == pmax:
-        dmax = pmax + 1
+    Z = np.empty((jmax,) + r.shape)
+    for j in range(jmax):
+        n, m = jtonm(j + 1)
+        const = sqrt((2 - (m == 0)) * (n + 1))
+        if m < 0:
+            Z[j] = R[n, -m] * sntheta[-m] * const
+        else:
+            Z[j] = R[n, m] * cntheta[m] * const
+    # Make zernike zero outside unit circle (useful for dot product)
+    Z = Z * np.less_equal(r, 1.0)
+    if orthoganalise:
+        return Orthoganalise(Z)
     else:
-        dmax = pmax
-    for q in range(dmax):
-        R[q, q] = np.power(r, q)
-    # Calc second batch of elements according to diagram in source paper
-    for q in range(dmax - 2):
+        return Z
+
+
+def KinterRadial(r, maxRadial):
+    """Compute Zernike radial functions using the modified Kintner method.
+
+    Discussion may be found in Chong, Chee-Way, P. Raveendran, and R. Mukundan. 
+    'A Comparative Analysis of Algorithms for Fast Computation of Zernike Moments'.
+    Pattern Recognition 36, no. 3 (March 2003): 731-42. 
+    doi:10.1016/S0031-3203(02)00091-2."""
+    # Storage array for result
+    # Creates array 4 * larger than necessary, but simpler to code
+    R = np.empty((maxRadial + 1, maxRadial + 1) + r.shape)
+    # Calc diagonal elements recursively
+    R[0, 0] = np.ones_like(r)
+    q = np.arange(1, maxRadial + 1)
+    R[q, q] = np.cumprod(np.broadcast_to(r, (maxRadial,) + r.shape), axis=0)
+    # Calc second batch of elements according to Fig 3 in Chong et al.
+    for q in range(maxRadial - 1):
         R[q + 2, q] = (q + 2) * R[q + 2, q + 2] - (q + 1) * R[q, q]
     # Calc remaining elements
-    for q in range(pmax - 2):
-        if q > qmax:
-            dmax = pmax
-        else:
-            dmax = 1 + pmax
-        for p in range(q + 4, dmax, 2):
+    r2 = r ** 2
+    for q in range(maxRadial - 3):
+        for p in range(q + 4, maxRadial + 1, 2):
             k1 = (p + q) * (p - q) * (p - 2) / 2.0
             k2 = 2 * p * (p - 1) * (p - 2)
             k3 = -q * q * (p - 1) - p * (p - 1) * (p - 2)
             k4 = -p * (p + q - 2) * (p - q - 2) / 2.0
-            R[p, q] = ((k2 * r ** 2 + k3) * R[p - 2, q] + k4 * R[p - 4, q]) / k1
+            R[p, q] = ((k2 / k1) * r2 + (k3 / k1)) * R[p - 2, q] + (k4 / k1) * R[
+                p - 4, q
+            ]
+    return R
 
-    # Fill Zernike array
-    for j in range(0, jmax):
-        nm = jtonm(j + 1)
-        n = nm[0]
-        m = nm[1]
-        p = n
-        q = int(np.absolute(m))
-        if m < 0:
-            Z[j] = R[p, q] * np.sin(q * phi)
-        else:
-            Z[j] = R[p, q] * np.cos(q * phi)
-        # Normalise
-        Z[j] *= sqrt((2 - (m == 0)) * (n + 1))
-    # Make zernike zero outside unit circle (useful for dot product)
-    Z = Z * np.less_equal(r, 1.0)
-    return Z
 
 def ZernikeHardCoded(gridSize, maxRadial, diameter=None):
     """
     Hardcoded Zernike routine: works only up to 5th radial order
     """
-    assert maxRadial<=5
+    assert maxRadial <= 5
     if diameter == None:
         diameter = gridSize
     radius = diameter / 2.0
-    x, y = np.mgrid[0:gridSize, 0:gridSize]
+    y, x = np.mgrid[0:gridSize, 0:gridSize]
     x = (x - (gridSize - 1.0) / 2.0) / radius
     y = (y - (gridSize - 1.0) / 2.0) / radius
     # Derive radius and exp(i*theta)
     temp = x + 1j * y
     r1 = np.abs(temp)
-    e1 = np.where(r1==0.0,1.0,temp / r1)
+    eitheta = np.where(r1 == 0.0, 1.0, temp / r1)
     # Generate powers of r recursively
     r2 = r1 * r1
     r3 = r2 * r1
     r4 = r3 * r1
     r5 = r4 * r1
     # Generate cos and sin terms recursively from exp(i*theta)
-    e2 = e1 * e1
-    e3 = e2 * e1
-    e4 = e3 * e1
-    e5 = e4 * e1
-    ctheta = e1.real
-    stheta = e1.imag
+    e2 = eitheta * eitheta
+    e3 = e2 * eitheta
+    e4 = e3 * eitheta
+    e5 = e4 * eitheta
+    ctheta = eitheta.real
+    stheta = eitheta.imag
     c2theta = e2.real
     s2theta = e2.imag
     c3theta = e3.real
@@ -158,42 +164,44 @@ def ZernikeHardCoded(gridSize, maxRadial, diameter=None):
     return zernike[: NumZernike(maxRadial)]
 
 
-def test_vs_hardcoded(gridSize=100,maxRadial=5):
-    Z1=ZernikeGrid(gridSize,maxRadial)
-    Z2=ZernikeHardCoded(gridSize,maxRadial)
-    diff = Z1 - Z2
-    print(np.sqrt(np.sum(diff**2,axis=(-1,-2))))
-
-
-
-def GramSchmidt(Zernikes, unitNorm=False):
-    """Gram-Schmidt orthoganalise a set of vectors.
-
-    Uses QR factorisation in numpy.
- 
-    unitNorm=True => ouput array will be orthonormal, otherwise the normalisation will be the same as for the input array"""
-
-    numZernikes = len(Zernikes)
-    diameter = len(Zernikes[0])
-    if diameter ** 2 < numZernikes:
-        raise ValueError(
-            "Orthogonalisation only implemented for diameter^2 > numZernikes"
-        )
-    # Reshape Zernike array into column vectors
-    colZernikes = np.reshape(
-        np.ravel(Zernikes, order="C"), (diameter ** 2, numZernikes), order="F"
-    )
+def Orthoganalise(Zernikes):
+    """Orthoganalise Zernikes to mitigate effects of under-sampling.
+    """
+    colZernikes = np.transpose(np.reshape(Zernikes, (len(Zernikes), -1)))
     q, r = np.linalg.qr(colZernikes, mode="reduced")
-    # Reshape column vectors back to array of meshgrids
-    orthoZernikes = np.reshape(
-        np.ravel(q, order="F"), (numZernikes, diameter, diameter), order="C"
-    )
-    if not unitNorm:
-        for i in np.arange(numZernikes):
-            zernikeNorm = sqrt(Sum2d(np.square(Zernikes[i])))
-            orthoZernikes[i] = np.multiply(orthoZernikes[i], zernikeNorm)
+    orthoZernikes = np.reshape(np.transpose(q), Zernikes.shape)
+    zernikeNorm = np.sqrt(np.sum(r ** 2, axis=0)) * np.sign(np.diag(r))
+    orthoZernikes *= zernikeNorm[:, None, None]
     return orthoZernikes
 
 
-if __name__ == "__main__":
-    test()
+def test_vs_hardcoded(gridSize=100, maxRadial=5):
+    Z1 = ZernikeGrid(gridSize, maxRadial, orthoganalise=False)
+    Z2 = ZernikeHardCoded(gridSize, maxRadial)
+    diff = Z1 - Z2
+    rms = np.sqrt(np.sum(diff ** 2, axis=(-1, -2)))
+    print(rms)
+    print(np.amax(rms))
+
+
+def test_ortho(gridSize=32, maxRadial=5):
+    Z = ZernikeGrid(gridSize, maxRadial)
+    orth, q, r = Orthoganalise(Z, debug=True)
+    print(q.shape, r.shape, orth.shape)
+    print((np.abs(r) > 1e-5).astype(np.int))
+    m = gridSize // 2
+    print(orth[:3, m - 2 : m + 2, m - 2 : m + 2])
+    diag = np.diag(r)
+    roff = r - np.diag(diag)
+    print(np.amax(roff))
+    print(diag ** 2)
+    print(np.sum(Z ** 2, axis=(-1, -2)) - np.sum(orth ** 2, axis=(-2, -1)))
+    print(np.sum(q ** 2, axis=0))
+    # print(np.sum(roff**2,axis=0))
+    # print(np.sum(r**2,axis=1))
+    # print(np.sum(r**2,axis=0))
+
+
+test_vs_hardcoded()
+# test_ortho()
+# Z=ZernikeGrid(gridSize=100, maxRadial=5)
